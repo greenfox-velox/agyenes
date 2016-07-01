761af487138c4ae6c2b499d10e255618ef3c335b
agyenes <aron.gyenes@gmail.com>
2016-07-01 15:03:00 +0200

    gallery working OK

--- week-07/day-4/gallery.js
+++ week-07/day-4/gallery.js
@@ -1,24 +1,72 @@
 'use strict';
 
+var images = [
+    "Images/img1.jpg",
+    "Images/img2.jpg",
+    "Images/img3.jpg",
+    "Images/img4.jpg",
+    "Images/img5.jpg",
+    "Images/img6.jpg",
+    "Images/img7.jpg",
+    "Images/img8.jpg",
+    "Images/img9.jpg",
+    "Images/img10.jpg",
+]
+
+var picture_titles = [
+    "1. The staircase",
+    "2. Broken glass",
+    "3. Stella playing pinball",
+    "4. Trelkovsky at the Seine",
+    "5. Monsieur Zy",
+    "6. Trelkovsky in the pub",
+    "7. A mysterious character in the story",
+    "8. Trelkovsky at the park",
+    "9. Madame Dioz & Monsieur Zy",
+    "10. Back to the staircase"
+]
+
 var imagecount = 1
-var total = 10
+var thumbnail_count = 0
+var total = images.length
+var arrowleft = document.querySelector('.selected .left_holder img');
+var arrowright = document.querySelector('.selected .right_holder img');
+var littlearrowleft = document.querySelector('.thumbnails .left_holder img');
+var littlearrowright = document.querySelector('.thumbnails .right_holder img');
+var visible_thumbnails = document.querySelectorAll('.little_pictures img');
+
+arrowleft.addEventListener('click', function() {slide(-1)});
+arrowright.addEventListener('click', function() {slide(1)});
+littlearrowleft.addEventListener('click', function() {step(-1)});
+littlearrowright.addEventListener('click', function() {step(1)});
+
 
 function slide (x) {
-  var Image = document.getElementById('img');
+  var Image = document.querySelector('.selected_image');
+  var imgTitle = document.querySelector('h3');
   imagecount = imagecount + x;
-  if (imagecount > total) { imagecount =1;}
+  if (imagecount > total) { imagecount = 1;}
   if (imagecount < 1) { imagecount = total;}
-  Image.src = "Images/img" + imagecount +".jpg";
+  Image.src = images[imagecount - 1];
+  imgTitle.textContent = picture_titles[imagecount-1]
 }
 
 function step (y) {
-  var little_images = document.querySelectorAll('.little_pictures');
-  imagecount = imagecount + y;
-  Image.src = "Images/img" + imagecount +".jpg";
+  if (thumbnail_count + y <= 4 && thumbnail_count + y >= 0) {
+      thumbnail_count = thumbnail_count + y;
+      for (var i = 0; i < 6; i++) {
+        visible_thumbnails[i].setAttribute('src', images[thumbnail_count + i]);
+    }
+  }
 }
 
-function highlight (y) {
-  var Image = document.getElementById('img');
-  var little_image = document.querySelectorAll('.little_pictures')[y];
-  Image = little_image;
+for (var i = 0; i < visible_thumbnails.length;i++) {
+  visible_thumbnails[i].addEventListener('click', changeImg);
+}
+
+function changeImg (event) {
+  var Image = document.querySelector('.selected_image');
+  Image.setAttribute('src', event.target.getAttribute('src'));
+  var imgTitle = document.querySelector('h3');
+  imgTitle.textContent = picture_titles[images.indexOf(Image.getAttribute('src'))]
 }
